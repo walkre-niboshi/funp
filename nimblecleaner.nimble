963348b3d0819781059f60a9031359875b81b852
walkre <boo880432@gmail.com>
2019-03-14 20:13:57 +0900

    とりあえず動くようにした

---
+++ nimblecleaner.nimble
@@ -0,0 +1,14 @@
+# Package
+
+version       = "0.1.0"
+author        = "walkre"
+description   = "Force uninstall a specified nimble package."
+license       = "MIT"
+srcDir        = "src"
+bin           = @["nimblecleaner"]
+
+
+# Dependencies
+
+requires "nim >= 0.19.4"
+requires "docopt >= 0.6.8"
---
+++ src/nimblecleaner.nim
@@ -0,0 +1,105 @@
+import os, osproc, ospaths, json, strscans, strutils
+import docopt
+
+proc removeFromJson(jsonNode: JsonNode, target: string): JsonNode =
+  if jsonNode.kind == JObject:
+    if jsonNode.contains("name") and jsonNode["name"].getStr() == target: return nil
+
+    result = newJObject()
+    for key, child in jsonNode.getFields().pairs:
+      let childResult = child.removeFromJson(target)
+      if childResult == nil: continue
+
+      result.add(key, child.removeFromJson(target))
+
+  elif jsonNode.kind == JArray:
+    result = newJArray()
+    for child in jsonNode.items:
+      let childResult = child.removeFromJson(target)
+      if childResult == nil: continue
+
+      result.add(child.removeFromJson(target))
+  else: return jsonNode
+
+proc promptContinuing() =
+  echo("Do you wish to continue? [y/N]")
+  let answer = readLine(stdin)
+  if answer != "y": quit()
+
+proc removeBinary(packageName, nimblePath: string) =
+  # Remove from bin directory
+  let binaryPath = nimblePath / packageName
+  try:
+    removeFile(binaryPath)
+    echo("Successfully removed the binary file: " & binaryPath)
+  except IOError:
+    echo("Failed to remove the binary file: " & binaryPath)
+    promptContinuing()
+
+proc removeFromNimbleDataJson(packageName, nimblePath: string) =
+  # Remove from nimbledata.json
+  let
+    nimbleDataJsonPath = nimblePath / "nimbledata.json"
+    jsonNode = parseFile(nimblePath / "nimbledata.json")
+  try:
+    let
+     packageRemovedJson = jsonNode.removeFromJson(packageName).pretty()
+    writeFile(nimbleDataJsonPath, packageRemovedJson)
+    echo("Successfully remove from nimbledata.json: " & nimbleDataJsonPath)
+  except JsonParsingError, IOError:
+    echo("Failed to remove from nimbledata.json: " & nimbleDataJsonPath)
+    promptContinuing()
+
+
+proc removePackageDirectories(packageName, nimblePath: string) =
+  # Remove from pkgs directory
+  let pkgsDirectory = nimblePath / "pkgs"
+  for kind, path in walkDir(pkgsDirectory):
+    if kind != PathComponent.pcDir: continue
+
+    let directoryName = path.extractFilename
+    if not directoryName.startsWith(packageName & "-"): continue
+
+    let versionInfo = directoryName.substr(packageName.len)
+    var v1, v2, v3: int
+    if scanf(versionInfo, "$i.$i.$i", v1, v2, v3):
+      try:
+        removeDir(path)
+        echo("Successfully removed package directory: " & path)
+      except IOError:
+        echo("Failed to remove package directory: " & path)
+        promptContinuing()
+
+const doc = """
+Usage:
+  nimblecleaner [--backup=<directory>] <package-name>
+  nimblecleaner (-h | --help)
+  nimblecleaner --version
+
+Options:
+  -h --help             Show this help message.
+  --version             Show version.
+  --backup=<directory>  Back up 'nimbledata.json' to the directory before removal.
+"""
+
+let args = docopt(doc)
+
+if args["--version"]:
+  echo("0.1.0")
+elif args["<package-name>"]:
+  let
+    packageName = $args["<package-name>"]
+    nimblePath = execProcess("nimble path " & packageName).parentDir.parentDir
+  
+  removeBinary(packageName, nimblePath)
+
+  if args["--backup"]:
+    try:
+      let nimbleDataJsonPath = nimblePath / "nimbledata.json"
+      copyFile(nimbleDataJsonPath, $args["--backup"] / "nimbledata.json")
+    except OSError:
+      echo("Failed to back up 'nimbledata.json'. Process cancelled.")
+      quit()
+
+  removeFromNimbleDataJson(packageName, nimblePath)
+  removePackageDirectories(packageName, nimblePath)
